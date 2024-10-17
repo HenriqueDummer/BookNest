b44c8edd1baa80141652e27b71c55b7cafe62ca2
Henrique Dummer <hatiehique2005@gmail.com>
2024-10-17 18:16:26 -0300

    Test render

--- frontend/src/util/http.js
+++ frontend/src/util/http.js
@@ -1,215 +1,224 @@
 import { QueryClient } from "@tanstack/react-query";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient();
 
 export const getMe = async () => {
-    try{
-        const res = await fetch("http://localhost:8080/api/auth/getme", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
+  try {
+    const res = await fetch(
+      "https://book-nest-api-silk.vercel.app/api/auth/getme",
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-        const data = await res.json() 
-        console.log(data)
-        if(data.error) return false
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
-
-export const submitSignUp = async (formData) => {
-    const {email, password, username} = formData
-
-    try{
-        const res = await fetch("http://localhost:8080/api/auth/signup", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email,
-                password,
-                username
-            })
-        })
-
-        const data = await res.json()
-        console.log(data)
-
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    const data = await res.json();
+    console.log(data);
+    if (data.error) return false;
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const submitLogIn = async (formData) => {
-    const {email, password} = formData
-    console.log(JSON.stringify({email, password}))
-    try{
-        const res = await fetch("http://localhost:8080/api/auth/login", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email,
-                password,
-            })
-        })
-        console.log(res)
-        const data = await res.json()
-        console.log(data)
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+  const { email, password } = formData;
+  console.log(JSON.stringify({ email, password }));
+  try {
+    const res = await fetch(
+      "https://book-nest-kt6irnr4i-henriquedummers-projects.vercel.app/api/auth/login",
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      }
+    );
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export const logout = async() => {
-    try{
-        const res = await fetch("http://localhost:8080/api/auth/logout", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
+export const submitSignUp = async (formData) => {
+  const { email, password, username } = formData;
 
-        const data = await res.json()
+  try {
+    const res = await fetch("http://localhost:8080/api/auth/signup", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+        username,
+      }),
+    });
 
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    const data = await res.json();
+    console.log(data);
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await fetch(
+      "https://book-nest-oqjzxysca-henriquedummers-projects.vercel.app/api/auth/logout",
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const data = await res.json();
+
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getAllBooks = async () => {
-    try{
-        const res = await fetch("http://localhost:8080/api/books/all", {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
+  try {
+    const res = await fetch("http://localhost:8080/api/books/all", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const data = await res.json() 
+    const data = await res.json();
 
-        console.log(data)
+    console.log(data);
 
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getBooksByStatus = async (status) => {
+  try {
+    const res = await fetch(
+      "http://localhost:8080/api/books/status/" + status,
+      {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    try{
-        const res = await fetch("http://localhost:8080/api/books/status/" + status, {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
+    const data = await res.json();
 
-        const data = await res.json() 
+    console.log(data);
 
-        console.log(data)
-
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const getBookById = async (id) => {
-    try{
-        const res = await fetch("http://localhost:8080/api/books/" + id, {
-            method: "GET",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-        })
+  try {
+    const res = await fetch("http://localhost:8080/api/books/" + id, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const data = await res.json() 
+    const data = await res.json();
 
-        console.log(data)
+    console.log(data);
 
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const addBook = async (formData) => {
-    try{
-        const res = await fetch("http://localhost:8080/api/books/create", {
-            method: "POST",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-               ...formData
-            })
-        })
+  try {
+    const res = await fetch("http://localhost:8080/api/books/create", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...formData,
+      }),
+    });
 
-        const data = await res.json()
-        console.log(data)
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const deleteBook = async (id) => {
-    try{
-        const res = await fetch("http://localhost:8080/api/books/delete/" + id, {
-            method: "DELETE",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
+  try {
+    const res = await fetch("http://localhost:8080/api/books/delete/" + id, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const data = await res.json()
-        console.log(data)
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const updateBook = async (formData) => {
+  const id = formData._id;
 
-    const id = formData._id
+  try {
+    const res = await fetch("http://localhost:8080/api/books/update/" + id, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        ...formData,
+      }),
+    });
 
-    
-    try{
-        const res = await fetch("http://localhost:8080/api/books/update/" + id, {
-            method: "PATCH",
-            credentials: "include",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-               ...formData
-            })
-        })
-
-        const data = await res.json()
-        console.log(data)
-        return data
-    }catch(err){
-        console.log(err)
-    }
-}
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
